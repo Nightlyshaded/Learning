d29d693551d35ce64d468c0c87db968911a57810
Ghost <notzodiac46@hotmail.com>
2024-08-17 21:25:18 +0200

    Responding to users number

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -6,7 +6,7 @@ int main()
 
 	std::cout << "Hello user.. Whats your favioute number" << std::endl;
 	std::cin >> FavNum;
-	std::cout << "Oooh cool number ;D" << std::endl;
+	std::cout << "Oooh cool " << FavNum << " It's also my fav number :D" << std::endl;
 	return 0;
 
 }
