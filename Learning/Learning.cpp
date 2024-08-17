448f93b626ae0e638d994dd3b7382088203bef5b
notzo <notzo@DESKTOP-20OH2M9>
2024-08-17 20:53:20 +0200

    Add project files.

---
+++ Learning/Learning.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int FavNum;
+
+	std::cout << "Hello user.. Whats your favioute number" << std::endl;
+	std::cin >> FavNum;
+	std::cout << "Oooh cool number ;D" << std::endl;
+	return 0;
+
+}
