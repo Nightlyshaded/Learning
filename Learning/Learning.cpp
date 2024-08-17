b6cc3246f48c793e80b88ae0ac309045295ffb29
Ghost <notzodiac46@hotmail.com>
2024-08-17 21:28:18 +0200

    Added comments

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -2,10 +2,13 @@
 
 int main()
 {
+
+	//stores users number
 	int FavNum;
 
 	std::cout << "Hello user.. Whats your favioute number" << std::endl;
 	std::cin >> FavNum;
+	//uses stored value and responds to user 
 	std::cout << "Oooh cool " << FavNum << " It's also my fav number :D" << std::endl;
 	return 0;
 
